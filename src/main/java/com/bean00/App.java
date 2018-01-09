05d94bb77719197dbea8b44f6153bb604933a63f
Jon Chin <jchin@8thlight.com>
2018-01-09 15:25:17 -0800

    Pass SimpleGet test

--- src/main/java/com/bean00/App.java
+++ src/main/java/com/bean00/App.java
@@ -1,13 +1,44 @@
 package com.bean00;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) throws IOException {
+        System.out.println( "Server started" );
+
+        // create a socket
+        ServerSocket serverSocket =
+                new ServerSocket(5000);
+        Socket clientSocket = serverSocket.accept();
+        PrintWriter out =
+                new PrintWriter(clientSocket.getOutputStream(), true);
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(clientSocket.getInputStream()));
+
+        // receive the request
+        String inputLine;
+
+        while (true) {
+            for (int i = 0; i < 6; i++) {
+                inputLine = in.readLine();
+                System.out.println(inputLine);
+            }
+
+            // returns a response
+            out.println("HTTP/1.1 200 OK\r");
+
+            break;
+        }
+
     }
 }
